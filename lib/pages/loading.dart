1588004446e7dd600a784504d7cf6352c47ec633
Blaise202 <iblaise2023@gmail.com>
2025-05-12 16:26:14 +0000

    async and http package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 
@@ -9,6 +11,26 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void  getData() async {
+    String username = await Future.delayed(Duration(seconds: 2), () {
+      return 'Blaise';
+    });
+
+    String bio = await Future.delayed(Duration(seconds: 3), () {
+      return 'Passionate software developer';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+    print('hello');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
