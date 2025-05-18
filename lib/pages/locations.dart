64eaa8b5208b58ce3013b8e98e0a3a02aa3e4c16
Blaise202 <iblaise2023@gmail.com>
2025-05-18 15:41:03 +0000

    locations cards

--- lib/pages/locations.dart
+++ lib/pages/locations.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:world_time/services/world_time.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -9,6 +9,20 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Kigali', location: 'Kigali', flag: 'rwanda.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'sk.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +39,29 @@ class _LocationState extends State<Location> {
         foregroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Text('Choose Locations here'),
+      body:
+      Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Card(
+                child: ListTile(
+                  onTap: (){
+
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
